87d7bbdd7b1e1ac42ec2ef34ee188063ab6f4b40
Lynx Pardelle <lynxpardelle@lynxpardelle.com>
2023-09-04 22:24:16 -0600

    sextaentrega.md

---
+++ TriggersMontano.sql
@@ -0,0 +1,75 @@
+USE blog;
+/* 
+  actions_log:
+  - action_id
+  - timestamp
+  - user_id
+  - action
+  - table_name
+  - user_name
+  - action_date
+  - action_time
+ */
+CREATE TABLE actions_log (
+    action_id INT AUTO_INCREMENT PRIMARY KEY,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id INT, -- ID del usuario que realizó la acción
+    action VARCHAR(255) NOT NULL, -- Descripción de la acción o evento
+    table_name VARCHAR(100), -- Nombre de la tabla relacionada con la acción (si es aplicable)
+    action_date DATE, -- Fecha de la acción
+    action_time TIME -- Hora de la acción
+);
+/* 
+    Triggers:
+*/
+/* 
+    El trigger before_users_insert se ejecuta antes de insertar 
+    un nuevo registro en la tabla users y se usa para crear 
+    un nuevo registro en la tabla actions_log.
+*/
+DELIMITER //
+
+CREATE TRIGGER before_users_insert
+BEFORE INSERT
+ON users FOR EACH ROW
+BEGIN
+    INSERT INTO actions_log (action, table_name)
+    VALUES ('Usuario creado', 'users');
+    INSERT INTO actions_log (action, table_name, action_date, action_time)
+    VALUES ('Usuario creado', 'users',  CURDATE(), CURTIME());
+END;
+// 
+  DELIMITER ;
+DELIMITER //
+
+-- Triggers para la tabla 'articles'
+/* 
+  Los triggers after_articles_insert, after_articles_update 
+  y after_articles_delete se ejecutan después de insertar, 
+  actualizar o eliminar un registro en la tabla articles 
+  y se usan para actualizar la vista all_articles.
+ */
+CREATE TRIGGER after_articles_insert
+AFTER INSERT
+ON articles FOR EACH ROW
+BEGIN
+    CALL refresh_all_articles_view();
+END;
+//
+
+CREATE TRIGGER after_articles_update
+AFTER UPDATE
+ON articles FOR EACH ROW
+BEGIN
+    CALL refresh_all_articles_view();
+END;
+//
+
+CREATE TRIGGER after_articles_delete
+AFTER DELETE
+ON articles FOR EACH ROW
+BEGIN
+    CALL refresh_all_articles_view();
+END;
+//
+  DELIMITER ;
---
+++ sextaentrega.md
@@ -0,0 +1,16 @@
+# Script de creación de triggers
+
+Presentar en formato .sql el script de creación de 2 triggers con base en los datos de la base de datos del proyecto final
+
+# Consigna
+En la base de datos de tu proyecto final, debes incluir al menos una tabla del tipo LOG, Bitácora o Movimientos. Elegir dos de las tablas más importantes donde se operan con registros de forma frecuente, y crearás dos Triggers en cada una de ellas.
+
+Los Triggers a crear deberán controlar la acción previo a la operación elegida (BEFORE), y una acción posterior a otra operación elegida (AFTER).
+
+# Aspectos a incluir
+- Agrega una explicación por cada trigger a crear, explicando qué controlará el mismo.
+- Recuerda agregar el script de creación de las tablas LOG.
+- Debe registrar el usuario que realizó la operación, la fecha, y la hora (en campos separados).
+
+# Formato
+El archivo a presentar debe ser del tipo .sql nombrado como “Triggers+Apellido”.
